ed3d09b66a56a3198fed136b5591836abfd5e403
msonumanish <msonu.manish@gmail.com>
2017-10-30 04:14:16 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,3 +1,4 @@
+# %load q04_decision_classifier_plot/build.py
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -17,5 +18,5 @@ depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
-
-
+def decision_classifier_plot (X_train, X_test, y_train, y_test,depths):
+    a=1
