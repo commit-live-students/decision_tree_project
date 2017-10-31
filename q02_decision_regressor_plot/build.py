f92ee60c55cfc2c9bb165f831e1232f7064e4ef6
satishkhanna <snykhanna@gmail.com>
2017-10-31 18:13:45 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -14,4 +14,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+y_test_frame= []
+x_test_frame = []
+y_training_frame = []
 
+def decision_regressor_plot (X_train,X_test,y_train,y_test,depths):
+    for i in depths:
+        dr = DecisionTreeRegressor(max_depth = 5)
+        dr.fit(X_train,y_train)
+        y_test_pred = dr.predict(X_test)
+      #  y_test_pred_list = np.ndarray.tolist(y_test_pred)
+        y = mean_squared_error(y_test,y_test_pred)
+        y_test_frame.append(y)
+        x_test_frame.append(i)
+        y_train_pred = dr.predict(X_train)
+        y_train_f = mean_squared_error(y_train,y_train_pred)
+        y_training_frame.append(y_train_f)
+    plt.plot(x_test_frame,y_test_frame)
+    plt.plot(x_test_frame,y_training_frame)
+
+    plt.show()
