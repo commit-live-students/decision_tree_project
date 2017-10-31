03e6c084cd33cba298dd53f6c7ef6a9eb4fef701
satishkhanna <snykhanna@gmail.com>
2017-10-31 18:20:33 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -17,5 +17,23 @@ depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
+y_test_frame= []
+x_test_frame = []
+y_training_frame = []
 
+def decision_classifier_plot (X_train,X_test,y_train,y_test,depths):
+    for i in depths:
+        dr = DecisionTreeClassifier(max_depth = 5)
+        dr.fit(X_train,y_train)
+        y_test_pred = dr.predict(X_test)
+      #  y_test_pred_list = np.ndarray.tolist(y_test_pred)
+        y = accuracy_score(y_test,y_test_pred)
+        y_test_frame.append(y)
+        x_test_frame.append(i)
+        y_train_pred = dr.predict(X_train)
+        y_train_f = accuracy_score(y_train,y_train_pred)
+        y_training_frame.append(y_train_f)
+    plt.plot(x_test_frame,y_test_frame)
+    plt.plot(x_test_frame,y_training_frame)
 
+    plt.show()
