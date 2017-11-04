1ce07ac425aa0a3659d6a4f3d742ce4d4ce397c7
samir0719 <sameer_0719@yahoo.com>
2017-11-04 16:29:57 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_decision_regressor_plot/build.py
 # default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -12,6 +13,20 @@ y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
-
+def decision_regressor_plot(X_train, X_test, y_train, y_test,depth_list):
+    test_score=[]
+    train_score=[]
 # Write your solution here :
-
+    for i in range(len(depth_list)):
+        model =DecisionTreeRegressor(max_depth=depth_list[i])
+        model.fit(X_train,y_train)
+        y_pred=model.predict(X_train)
+        y_pred_test=model.predict(X_test)
+        train_score.insert(i,mean_squared_error(y_train,y_pred))
+        test_score.insert(i,mean_squared_error(y_test,y_pred_test))
+    plt.plot(depth_list, train_score, 'b', label='train set')
+    plt.plot(depth_list, test_score, 'g', label='test set')
+    plt.legend(loc='best')
+    plt.xlabel('depth')
+    plt.ylabel('mean_square_error')
+    plt.show()
