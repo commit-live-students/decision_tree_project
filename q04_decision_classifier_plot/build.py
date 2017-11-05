c4d0374ddd9acc6901864e4a592a3811f22578d5
samir0719 <sameer_0719@yahoo.com>
2017-11-05 04:13:15 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,21 +1,31 @@
 # default imports
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("./data/loan_prediction.csv")
-np.random.seed(9)
+data = pd.read_csv("./data/house_pricing.csv")
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
-
-
+depth_list =  [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
+def decision_classifier_plot(X_train, X_test, y_train, y_test,depth_list):
+    test_score=[]
+    train_score=[]
 # Write your solution here :
-
-
+    for i in range(len(depth_list)):
+        model =DecisionTreeClassifier(max_depth=depth_list[i])
+        model.fit(X_train,y_train)
+        y_pred=model.predict(X_train)
+        y_pred_test=model.predict(X_test)
+        train_score.insert(i,accuracy_score(y_train,y_pred))
+        test_score.insert(i,accuracy_score(y_test,y_pred_test))
+    plt.plot(depth_list, train_score, 'b', label='train set')
+    plt.plot(depth_list, test_score, 'g', label='test set')
+    plt.legend(loc='best')
+    plt.xlabel('depth')
+    plt.ylabel('accuracy_score')
+    plt.show()
