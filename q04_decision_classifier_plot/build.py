9b5cd89038f9de97037c4414ac63143effc68823
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-11-13 12:11:29 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,3 +1,4 @@
+# %load q04_decision_classifier_plot/build.py
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -13,9 +14,11 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
-
+depths = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 # Write your solution here :
+def decision_classifier_plot (X_train, X_test, y_train, y_test,depths):
+    a=1
 
 
+#decision_classifier_plot (X_train, X_test, y_train, y_test,depths)
