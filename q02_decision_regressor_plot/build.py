4f90354ba97812544e785b9a569bba42db54464b
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-11-13 08:40:27 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_decision_regressor_plot/build.py
 # default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -11,7 +12,25 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
+depths = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+def decision_regressor_plot (X_train, X_test, y_train, y_test,depths):
+    trl=[]
+    tsl=[]
+    for i in depths:
 
+        ds=DecisionTreeRegressor(max_depth=i)
+        ds.fit(X_train,y_train)
+        trainres=ds.predict(X_train)
+
+        msetrain=mean_squared_error(y_train,trainres)
+        trl.append(msetrain)
+        testres=ds.predict(X_test)
+        msetest=mean_squared_error(y_test,testres)
+        tsl.append(msetest)
+
+    plt.plot(depths,tsl,c='r')
+    plt.plot(depths,trl,c='y')
+
+#decision_regressor_plot (X_train, X_test, y_train, y_test,depths)
