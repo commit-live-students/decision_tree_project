a0094c026b7a484217c9817fea5befb5aef07f25
rohanhg91 <rohanhgupta91@gmail.com>
2017-11-19 11:05:39 +0000

    Done

--- q03_my_decision_classifier/build.py
+++ q03_my_decision_classifier/build.py
@@ -18,3 +18,11 @@ param_grid = {"max_depth": [8, 10, 15, 20],
 
 
 # Write your solution here :
+def my_decision_classifier(X_train,X_test,y_train,y_test,param_grid,n_iter_search=10):
+    clf = DecisionTreeClassifier(random_state=9)
+    rand_search = RandomizedSearchCV(estimator = clf,param_distributions = param_grid,n_iter=n_iter_search)
+    rand_search.fit(X_train,y_train)
+    y_pred = rand_search.predict(X_test)
+    best_param = rand_search.best_params_
+    acc = accuracy_score(y_test,y_pred)
+    return acc, best_param
