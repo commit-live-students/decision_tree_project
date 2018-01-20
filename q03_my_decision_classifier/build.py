4055a0f620da046edf2ed2bacd0b9e594e902451
brockLesnar37 <ashlin+37@greyatom.com>
2018-01-20 09:35:54 +0000

    Done

--- q03_my_decision_classifier/build.py
+++ q03_my_decision_classifier/build.py
@@ -1,3 +1,4 @@
+
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -6,15 +7,26 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-param_grid = {"max_depth": [8, 10, 15, 20],
-              "max_leaf_nodes": [2, 5, 9, 15, 20],
-              "max_features": [1, 2, 3, 5]}
+param_grid = {'max_depth': [8, 10, 15, 20],
+              'max_leaf_nodes': [2, 5, 9, 15, 20],
+              'max_features': [1, 2, 3, 5]}
 
 
 # Write your solution here :
+
+def my_decision_classifier(X_train, X_test, y_train, y_test, param_grid, n_iter_search=10):
+    decision_class = DecisionTreeClassifier(random_state=9)
+    rs_cv = RandomizedSearchCV(decision_class, param_distributions=param_grid, n_iter=n_iter_search)
+    rs_cv_fitted = rs_cv.fit(X_train, y_train)
+    best_params = rs_cv.best_params_
+    y_pred = rs_cv.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy, best_params
+
+
