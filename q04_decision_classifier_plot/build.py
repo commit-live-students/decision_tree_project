b98b09af36051c79687e2d7046ab49c26556bb12
cchopade <cchopade@gmail.com>
2018-02-27 05:10:59 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -14,7 +14,23 @@ y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
 depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
+def decision_classifier_plot(X_train, X_test, y_train, y_test, depths):
+    acc_train = []
+    acc_test = []
 
+    for d in depths:
+        clf = DecisionTreeClassifier(random_state = 9, max_depth = d)
+        clf.fit(X_train, y_train)
+
+        acc_train.append(accuracy_score(y_train, clf.predict(X_train)))
+        acc_test.append(accuracy_score(y_test, clf.predict(X_test)))
+
+    fig, ax = plt.subplots()
+    ax.plot(depth_list, acc_train, label = 'Train Set')
+    ax.plot(depth_list, acc_test, label = 'Test Set')
+    ax.legend()
+    plt.xlabel('Depth')
+    plt.ylabel('Accuracy Score')
+    plt.show()
 
 # Write your solution here :
-
