2cebe47e91abcacc6047a3c8732d269dd32dc2e7
Alex19427 <alex19427@gmail.com>
2018-03-08 10:47:38 +0530

    update

--- q02_decision_regressor_plot/tests/test_q02_decision_regressor_plot.py
+++ q02_decision_regressor_plot/tests/test_q02_decision_regressor_plot.py
@@ -6,12 +6,13 @@ from ..build import decision_regressor_plot
 class TestDecision_regressor_plot(TestCase):
     def test_decision_regressor_plot_arguments(self):
 
-        # Input parameters tests
+    # Input parameters tests
         args = getargspec(decision_regressor_plot)
         self.assertEqual(len(args[0]), 5, "Expected arguments %d, Given %d" % (5, len(args[0])))
     def test_decision_regressor_plot_defaults(self):
-    	args = getargspec(decision_regressor_plot)
+        args = getargspec(decision_regressor_plot)
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
+
 
         # Return type tests
         # Nothing to check here
--- q04_decision_classifier_plot/tests/test_q04_decision_classifier_plot.py
+++ q04_decision_classifier_plot/tests/test_q04_decision_classifier_plot.py
@@ -6,11 +6,11 @@ from ..build import decision_classifier_plot
 class TestDecision_classifier_plot(TestCase):
     def test_decision_classifier_plot_arguments(self):
 
-        # Input parameters tests
+    # Input parameters tests
         args = getargspec(decision_classifier_plot)
         self.assertEqual(len(args[0]), 5, "Expected arguments %d, Given %d" % (5, len(args[0])))
     def test_decision_classifier_plot_defaults(self):
-    	args= getargspec(decision_classifier_plot)
+        args= getargspec(decision_classifier_plot)
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
         # Return type tests
