23e6b1a95d21ad5ee575406497849ebb11557f6c
dharmil25 <dharmil2524_shah@ymail.com>
2018-04-06 09:55:50 +0000

    Done

--- q01_my_decision_regressor/build.py
+++ q01_my_decision_regressor/build.py
@@ -15,3 +15,10 @@ param_grid = {"max_depth": [2, 3, 5, 6, 8, 10, 15, 20, 30, 50],
               "max_features": [4, 8, 20, 25]}
 
 # Write your solution here :
+def my_decision_regressor(X_train, X_test, y_train, y_test, param_grid):
+    decision_tree = DecisionTreeRegressor(random_state=9)
+    grid_search = GridSearchCV(estimator=decision_tree, param_grid=param_grid,cv=5)
+    grid_search.fit(X_train,y_train)
+    y_prediction = grid_search.predict(X_test)
+
+    return r2_score(y_test, y_prediction),grid_search.best_params_
