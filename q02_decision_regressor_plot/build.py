f18f80311c78f2202fb4f21b3d37b02d74928de7
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-04-17 12:06:45 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,3 +16,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+def decision_regressor_plot(X_train, X_test, y_train, y_test, depths):
+    mse = []
+    for i in range(0,len(depths)):
+        model = DecisionTreeRegressor(random_state=9, max_depth=depths[i])
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse.append(mean_squared_error(y_test, y_pred))
+
+    plt.plot(X=depths, y=mse)
+    plt.show()
