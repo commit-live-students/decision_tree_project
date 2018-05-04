68af0d7c3b47e16de730e996543772d18fbc3b5e
kvin15 <vinayakkhamkar@gmail.com>
2018-05-04 11:51:28 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -14,3 +14,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+def decision_regressor_plot(X_train, X_test, y_train, y_test, depth_list):
+    trainscore_lst = list()
+    testscore_lst = list()
+    for val in depth_list:
+        dtree = DecisionTreeRegressor(random_state=9, max_depth=val)
+        dtree.fit(X_train, y_train)
+        # train score
+        y_pred_train = dtree.predict(X_train)
+        train_score = mean_squared_error(y_true=y_train, y_pred=y_pred_train)
+        trainscore_lst.append(train_score)
+        # test score
+        y_pred_test = dtree.predict(X_test)
+        test_score = mean_squared_error(y_true=y_test, y_pred=y_pred_test)
+        testscore_lst.append(test_score)
+
+    train_mse = plt.plot(depth_list, trainscore_lst, color='b',label='Train_MSE')
+    test_mse = plt.plot(depth_list, testscore_lst, color='g',label='Test_MSE')
+    plt.xlabel('Tree Depth')
+    plt.ylabel('Mean Squared Error')
+    plt.legend()
+    plt.show()
