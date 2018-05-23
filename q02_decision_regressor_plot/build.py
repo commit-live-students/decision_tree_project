49e7ea39ce62e5abe5307b6c85b947175a42db22
santoshmayekar <santosh.mayekar@outlook.com>
2018-05-23 09:45:03 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,16 +1,31 @@
-# default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-data = pd.read_csv("./data/house_pricing.csv")
+data = pd.read_csv('./data/house_pricing.csv')
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
+depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 65, 80]
+def decision_regressor_plot(X_train, X_test, y_train, y_test, depths):
+    mean_test_scores = []
+    mean_train_scores = []    
+    for depth in depths:
+        dt_regressor = DecisionTreeRegressor(max_depth=depth)
+        dt_regressor.fit(X_train, y_train)
+        mse_train = mean_squared_error(y_train, dt_regressor.predict(X_train))
+        mse_test = mean_squared_error(y_test, dt_regressor.predict(X_test))
+        mean_test_scores.append(mse_test)
+        mean_train_scores.append(mse_train)        
+    plt.figure(figsize=(10, 6))
+    plt.plot(depths, mean_train_scores, c='b', label='Train set')
+    plt.plot(depths, mean_test_scores, c='g', label='Test set')
+    plt.legend(loc='upper left')
+    plt.xlabel('depths')
+    plt.ylabel('mean square error')
+    plt.show()
 
-depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
-# Write your solution here :
+
--- q03_my_decision_classifier/build.py
+++ q03_my_decision_classifier/build.py
@@ -6,15 +6,26 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-param_grid = {"max_depth": [8, 10, 15, 20],
-              "max_leaf_nodes": [2, 5, 9, 15, 20],
-              "max_features": [1, 2, 3, 5]}
+param_grid = {'max_depth': [8, 10, 15, 20],
+              'max_leaf_nodes': [2, 5, 9, 15, 20],
+              'max_features': [1, 2, 3, 5]}
+
+def my_decision_classifier(X_train1,X_test1,y_train1,y_test1,params,n=10):
+    X_train1,X_test1,y_train1,y_test1 = X_train, X_test, y_train, y_test
+    params = param_grid    
+    dt = DecisionTreeClassifier(random_state=9)
+    dt_rnd = RandomizedSearchCV(dt,param_distributions=params,n_iter=n)
+    dt_rnd.fit(X_train1,y_train1)
+    y_pred=dt_rnd.predict(X_test1)
+    score=accuracy_score(y_test1,y_pred)
+    return score,dt_rnd.best_params_
+
+my_decision_classifier(X_train,X_test,y_train,y_test,param_grid,n=10)
 
 
-# Write your solution here :
