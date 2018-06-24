1717d07f6fa023aba292cb167ea077d223f265d0
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-06-24 12:53:09 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,4 +1,4 @@
-# default imports
+
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("./data/house_pricing.csv")
+data = pd.read_csv('./data/house_pricing.csv')
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
@@ -14,3 +14,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+def decision_regressor_plot(X_train, X_test, y_train, y_test,depths):
+    mean_squared_error_train_list=[]
+    mean_squared_error_test_list=[]
+    for i in depth_list:
+        clf=DecisionTreeRegressor(max_depth=i)
+        clf.fit(X_train,y_train)
+        y_test_predictions=clf.predict(X_test)
+        y_train_predictions=clf.predict(X_train)
+        
+        mean_squared_error_train_list.append(mean_squared_error(y_train,y_train_predictions))
+        mean_squared_error_test_list.append(mean_squared_error(y_test,y_test_predictions))
+    plt.plot(depths,mean_squared_error_train_list)
+    plt.plot(depths,mean_squared_error_test_list)
+    plt.show()
+        
+
+#decision_regressor_plot(X_train, X_test, y_train, y_test,depth_list)
+        
+        
+
