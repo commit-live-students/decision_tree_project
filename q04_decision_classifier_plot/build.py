1883b7c9caf9437644693dcf802bb3b343e9a551
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-06-24 13:17:52 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,3 +1,4 @@
+# %load q04_decision_classifier_plot/build.py
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
@@ -17,3 +18,24 @@ depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
+def decision_classifier_plot(X_train, X_test, y_train, y_test,depths):
+    mean_train_score=[]
+    mean_test_score=[]
+    for depth in depths:
+        
+        clf=DecisionTreeClassifier(max_depth=depth)
+        clf.fit(X_train,y_train)
+        y_train_predictions=clf.predict(X_train)
+        y_test_predictions=clf.predict(X_test)
+        mean_train_score.append(accuracy_score(y_train,y_train_predictions))
+        mean_test_score.append(accuracy_score(y_test,y_test_predictions))
+    plt.plot(depths,mean_train_score)
+    plt.plot(depths,mean_test_score)
+    plt.show()
+
+#decision_classifier_plot(X_train,X_test,y_train,y_test,depth_list)
+    
+        
+        
+
+
