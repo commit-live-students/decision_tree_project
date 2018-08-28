3b5034c3471ac477dbb50e5d9ff7b7e96094dbde
Vagus30 <vishwas.tejaswi@gmail.com>
2018-08-28 20:58:51 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -15,20 +15,28 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
+depth = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
-def decision_classifier_plot(X_train,X_test,y_train,y_test,depths = depth_list):
-    clf = DecisionTreeClassifier(random_state=9)
-    cv = RandomizedSearchCV(clf,param_distribution=depth_list,n_iter=10)
-    cv.fit(X_train,y_train)
-    y_pred = cv.predict(X_test)
-    score = accuracy_score(y_test,y_pred).mean()
-    score_train = accuracy_score(y_train,y_pred)
-    plt.plot(depth_list,score)
-    plt.plot(score,depth_list)
-    plt.plot(score_train,depth_list)
+def decision_classifier_plot(X_train, X_test, y_train, y_test, depths):
+    mean_test_scores = []
+    mean_train_scores = []
+    
+    for depth in depths:
+        dt_classifier = DecisionTreeClassifier(max_depth=depth)
+        dt_classifier.fit(X_train, y_train)
+        acc_train = accuracy_score(y_train, dt_classifier.predict(X_train))
+        acc_test = accuracy_score(y_test, dt_classifier.predict(X_test))
+        mean_test_scores.append(acc_test)
+        mean_train_scores.append(acc_train)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(depths, mean_train_scores, c='b', label='Train set')
+    plt.plot(depths, mean_test_scores, c='g', label='Test set')
+    plt.legend(loc='upper left')
+    plt.xlabel('depths')
+    plt.ylabel('mean square error')
     plt.show()
-
+  
 
