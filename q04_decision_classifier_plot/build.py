b445b696fd8e3a40449f9129d4eed13bbd144b4f
abkedar <anupbhande@gmail.com>
2018-09-13 06:01:04 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 plt.switch_backend('agg')
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
@@ -16,5 +16,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
-
 # Write your solution here :
+
+def decision_classifier_plot(X_train, X_test, y_train, y_test, depths):
+    lst  = []
+    for depth in depths:
+        tree_reg = DecisionTreeClassifier(max_depth=depth,random_state=9)
+        tree_reg.fit(X_train, y_train)
+        y_pred_test=tree_reg.predict(X_test)
+        y_pred_train=tree_reg.predict(X_train)
+        lst.append((depth,accuracy_score(y_train,y_pred_train),accuracy_score(y_test,y_pred_test)))
+    df =  pd.DataFrame(lst)
+    plt.plot(df.iloc[:,0],df.iloc[:,1],c='r',label = 'Train')
+    plt.plot(df.iloc[:,0],df.iloc[:,2],c='g',label = 'Test')
+    plt.legend()
+    #plt.show()
+    
+decision_classifier_plot(X_train, X_test, y_train, y_test, depths=depth_list)
+
+
+
