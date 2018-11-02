5839807e338c05b227cdc09b2cd49b68ea11930c
rushabh-mehta <rushabh123@gmail.com>
2018-11-02 11:06:01 +0000

    Done

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_decision_regressor_plot/build.py
 # default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.switch_backend('agg')
 
-data = pd.read_csv("./data/house_pricing.csv")
+data = pd.read_csv('./data/house_pricing.csv')
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
@@ -15,3 +16,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50, 80]
 
 # Write your solution here :
+def decision_regressor_plot(X_train,X_test,y_train,y_test,depths):
+    mean_square_errors_test = []
+    mean_square_errors_train = []
+    for depth in depths:
+        tree_regressor = DecisionTreeRegressor(max_depth=depth)
+        tree_regressor.fit(X_train,y_train)
+        y_pred_train = tree_regressor.predict(X_train)
+        y_pred_test = tree_regressor.predict(X_test)
+        mean_square_errors_test.append(mean_squared_error(y_test,y_pred_test))
+        mean_square_errors_train.append(mean_squared_error(y_train,y_pred_train))
+    plt.plot(depth_list,mean_square_errors_train)
+    plt.plot(depth_list,mean_square_errors_test)
+decision_regressor_plot(X_train,X_test,y_train,y_test,depth_list)
+plt.show()
+
+
