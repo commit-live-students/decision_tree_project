304368a0b1dc229e7d13f30742609e7cefb8af15
sannidh <soniknitr@gmail.com>
2018-12-01 10:57:08 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,3 +1,4 @@
+# %load q04_decision_classifier_plot/build.py
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -8,7 +9,7 @@ import pandas as pd
 import numpy as np
 plt.switch_backend('agg')
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
@@ -17,4 +18,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
-# Write your solution here :
+def decision_classifier_plot(X_train, X_test, y_train, y_test, depths):
+    acc_train, acc_test = [], []
+    for i in depths:
+        model = DecisionTreeClassifier(max_depth=i)
+        model.fit(X_train, y_train)
+        y_pred_train = model.predict(X_train)
+        y_pred_test = model.predict(X_test)
+        acc_train.append(accuracy_score(y_train, y_pred_train))
+        acc_test.append(accuracy_score(y_test,y_pred_test))
+    plt.figure(figsize=(10,6))
+    plt.plot(depths, acc_train, 'b-', label='Train Set')
+    plt.plot(depths, acc_test, 'g.-', label='Test Set')
+    plt.xlabel('depths')
+    plt.ylabel('accuracy score')
+    plt.legend()
+
+
+
