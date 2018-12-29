d3acd95cf5603e51a50f504cf98ca9247f5096b5
rushabh-mehta <rushabh123@gmail.com>
2018-12-29 08:31:33 +0000

    Done

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -1,3 +1,4 @@
+# %load q04_decision_classifier_plot/build.py
 # default imports
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -8,7 +9,7 @@ import pandas as pd
 import numpy as np
 plt.switch_backend('agg')
 
-data = pd.read_csv("./data/loan_prediction.csv")
+data = pd.read_csv('./data/loan_prediction.csv')
 np.random.seed(9)
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
@@ -18,3 +19,15 @@ depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
+def decision_classifier_plot(X_train,X_test,y_train,y_test,depths):
+    train_scores=[]
+    test_scores=[]
+    for depth in depths:
+        tree_clf = DecisionTreeClassifier(max_depth=depth)
+        tree_clf.fit(X_train,y_train)
+        train_scores.append(tree_clf.score(X_train,y_train))
+        test_scores.append(tree_clf.score(X_test,y_test))
+    return plt.plot(depths,train_scores),plt.plot(depths,test_scores)
+decision_classifier_plot(X_train,X_test,y_train,y_test,depth_list)
+
+
